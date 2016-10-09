72b98166ee492ac1f3f8bc06bf63f266e10e9535
akm <akm2000@gmail.com>
2016-10-09 22:55:12 +0900

    Remove unused argument

--- lib/model_base/column_attribute.rb
+++ lib/model_base/column_attribute.rb
@@ -5,7 +5,7 @@ module ModelBase
     attr_accessor :linkable
     alias_method :linkable?, :linkable
 
-    def initialize(name, type, linkable: false, index_type: false, attr_options: {})
+    def initialize(name, type, index_type: false, attr_options: {})
       super(name, type, index_type, attr_options)
     end
   end
