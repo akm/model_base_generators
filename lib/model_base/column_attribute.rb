94fce2719b05b0ab3665ff942823e0cc97d21b2d
akm <akm2000@gmail.com>
2016-10-11 07:14:19 +0900

    Keep column to get null in order to render select

--- lib/model_base/column_attribute.rb
+++ lib/model_base/column_attribute.rb
@@ -6,10 +6,18 @@ module ModelBase
     alias_method :linkable?, :linkable
 
     attr_reader :reference
+    attr_reader :column
 
-    def initialize(name, type, reference: nil, index_type: false, attr_options: {})
+    class << self
+      def from_col(col, reference: nil)
+        ColumnAttribute.new(col.name, col.type, column: col, reference: reference)
+      end
+    end
+
+    def initialize(name, type, column: nil, reference: nil, index_type: false, attr_options: {})
       super(name, type, index_type, attr_options)
       @reference = reference
+      @column = column
     end
 
     def ref_model
@@ -17,6 +25,28 @@ module ModelBase
         @ref_model = reference.nil? ? nil : ModelBase::MetaModel.new(reference.class_name)
       end
       @ref_model
+    end
+
+    def selectable?
+      !!ref_model
+    end
+
+    def select_choices_expression(taregt_name)
+      query =
+        ref_model.respond_to?(:choices_for) ?
+          "#{ref_model.name}.choices_for(#{taregt_name})" :
+          "#{ref_model.name}.all"
+      "options_from_collection_for_select(#{qeury}, :id, :#{ref_model.title_column.name}x)"
+    end
+
+    def select_options
+      r = {}
+      r[:include_blank] = !required?
+      r
+    end
+
+    def required?
+      !column.try(:null)
     end
   end
 
