2a736c52c67cbd46fa80301f9fa9fb9e7a5b509f
akm <akm2000@gmail.com>
2016-10-13 23:33:55 +0900

    Add an example

--- spec/generators/rspec/scaffold_generator_spec.rb
+++ spec/generators/rspec/scaffold_generator_spec.rb
@@ -8,7 +8,7 @@ describe Rspec::Generators::ScaffoldGenerator, type: :generator do
 
   before { prepare_destination }
 
-  it 'creates a test initializer' do
+  it 'issues' do
     run_generator %w(issues)
 
     assert_expectation_file 'spec/controllers/issues_controller_spec.rb'
@@ -17,5 +17,16 @@ describe Rspec::Generators::ScaffoldGenerator, type: :generator do
     assert_expectation_file 'spec/views/issues/index.html.erb_spec.rb'
     assert_expectation_file 'spec/views/issues/new.html.erb_spec.rb'
     assert_expectation_file 'spec/views/issues/show.html.erb_spec.rb'
+  end
+
+  it 'projects' do
+    run_generator %w(projects)
+
+    assert_expectation_file 'spec/controllers/projects_controller_spec.rb'
+    assert_expectation_file 'spec/routing/projects_routing_spec.rb'
+    assert_expectation_file 'spec/views/projects/edit.html.erb_spec.rb'
+    assert_expectation_file 'spec/views/projects/index.html.erb_spec.rb'
+    assert_expectation_file 'spec/views/projects/new.html.erb_spec.rb'
+    assert_expectation_file 'spec/views/projects/show.html.erb_spec.rb'
   end
 end
