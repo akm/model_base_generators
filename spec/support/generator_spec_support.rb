c6db6f1ce7691bc81b4bb003ceaff670a56c7b48
akm <akm2000@gmail.com>
2016-10-14 01:14:13 +0900

    Remove unnecessary replacing

--- spec/support/generator_spec_support.rb
+++ spec/support/generator_spec_support.rb
@@ -2,7 +2,6 @@ module GeneratorSpecSupport
 
   def assert_expectation_file(path, expectation_filepath = nil)
     expectation_filepath ||= File.expand_path("../../../examples/#{path}", __FILE__)
-    expectation_filepath.sub!(/_spec\.rb\z/, '_spek.rb')
     assert_file(path, File.read(expectation_filepath))
   end
 
