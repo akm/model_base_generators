84847982ff902fa16a943dec9f8d5140643afdc9
akm <akm2000@gmail.com>
2016-10-13 13:47:51 +0900

    Use rspec-its

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,5 +8,7 @@ require File.expand_path("../../spec/dummy/config/environment.rb", __FILE__)
 ActiveRecord::Migrator.migrations_paths = [File.expand_path("../../spec/dummy/db/migrate", __FILE__)]
 # require "rails/test_help"
 
+require 'rspec/its'
+
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "model_base"
