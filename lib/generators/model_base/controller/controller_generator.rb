de5a777d6580ff96439d7d33d2e788c76983ea77
akm <akm2000@gmail.com>
2016-10-13 19:12:02 +0900

    Remove unused definitions

--- lib/generators/model_base/controller/controller_generator.rb
+++ lib/generators/model_base/controller/controller_generator.rb
@@ -15,14 +15,10 @@ module ModelBase
       class_option :api, type: :boolean,
                          desc: "Generates API controller"
 
-      argument :attributes, type: :array, default: [], banner: "field:type field:type"
-
       def create_controller_files
         template_file = options.api? ? "api_controller.rb.erb" : "controller.rb.erb"
         template template_file, File.join('app/controllers', controller_class_path, "#{controller_file_name}_controller.rb")
       end
-
-      hook_for :template_engine, :test_framework, as: :scaffold
 
       # Invoke the helper using the controller name (pluralized)
       hook_for :helper, as: :scaffold do |invoked|
