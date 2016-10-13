36ff51621e3da428c1e496225cecd1de9183ee04
akm <akm2000@gmail.com>
2016-10-13 23:18:23 +0900

    Use assert_expectation_file

--- spec/generators/erb/scaffold_generator_spec.rb
+++ spec/generators/erb/scaffold_generator_spec.rb
@@ -11,34 +11,20 @@ describe Erb::Generators::ScaffoldGenerator, type: :generator do
   it 'creates a test initializer' do
     run_generator %w(projects)
 
-    assert_file 'app/views/projects/_form.html.erb', /\<\%\= form_for.*\%\>/
-    assert_file 'app/views/projects/edit.html.erb', /render 'projects\/form'/
-    assert_file 'app/views/projects/index.html.erb', /\<table.*\>/
-    assert_file 'app/views/projects/new.html.erb', /render 'projects\/form'/
-    assert_file 'app/views/projects/show.html.erb', /\<dl .*\>/
+    assert_expectation_file 'app/views/projects/_form.html.erb'
+    assert_expectation_file 'app/views/projects/edit.html.erb'
+    assert_expectation_file 'app/views/projects/index.html.erb'
+    assert_expectation_file 'app/views/projects/new.html.erb'
+    assert_expectation_file 'app/views/projects/show.html.erb'
   end
 
   it 'creates a test initializer' do
     run_generator %w(issues)
 
-    assert_file 'app/views/issues/_form.html.erb' do |c|
-      expect(c).to match /\<\%\= form_for.*\%\>/
-      expect(c).to include '<%= f.collection_select :project_id, Project.all, :id, :name, {}, :class=>"form-control" %>'
-      expect(c).to include '<%= f.select :status, Issue.status.optoins, {}, :class=>"form-control" %>'
-    end
-    assert_file 'app/views/issues/edit.html.erb', /render 'issues\/form'/
-    assert_file 'app/views/issues/index.html.erb' do |c|
-      expect(c).to match /\<table.*\>/
-      expect(c).to include '<%= link_to issue.title, issue_path(issue) %>'
-      expect(c).to include '<%= issue.project.name %>'
-      expect(c).to include '<%= issue.status_text %>'
-    end
-    assert_file 'app/views/issues/new.html.erb', /render 'issues\/form'/
-    assert_file 'app/views/issues/show.html.erb' do |c|
-      expect(c).to match /\<dl .*\>/
-      expect(c).to include '<%= @issue.title %>'
-      expect(c).to include '<%= @issue.project.name %>'
-      expect(c).to include '<%= @issue.status_text %>'
-    end
+    assert_expectation_file 'app/views/issues/_form.html.erb'
+    assert_expectation_file 'app/views/issues/edit.html.erb'
+    assert_expectation_file 'app/views/issues/index.html.erb'
+    assert_expectation_file 'app/views/issues/new.html.erb'
+    assert_expectation_file 'app/views/issues/show.html.erb'
   end
 end
