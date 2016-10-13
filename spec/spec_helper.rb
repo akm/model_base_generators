c6dfb9eea8a49c29c0bde93977f1ea7a950f90f2
akm <akm2000@gmail.com>
2016-10-13 11:22:43 +0900

    Add config to load spec/dummy

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,13 @@
+require "simplecov"
+SimpleCov.start
+
+# Configure Rails Environment
+ENV["RAILS_ENV"] = "test"
+
+require File.expand_path("../../test/dummy/config/environment.rb", __FILE__)
+ActiveRecord::Migrator.migrations_paths = [File.expand_path("../../test/dummy/db/migrate", __FILE__)]
+ActiveRecord::Migrator.migrations_paths << File.expand_path('../../db/migrate', __FILE__)
+require "rails/test_help"
+
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "model_base"
