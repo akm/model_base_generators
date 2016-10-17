7ac7905601adcdf8ae0fbd7ccd57d72bdaf313d2
akm <akm2000@gmail.com>
2016-10-17 22:43:59 +0900

    Fix setting up test DB

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,7 +8,11 @@ ENV["RAILS_ENV"] = "test"
 
 require File.expand_path("../../spec/dummy/config/environment.rb", __FILE__)
 ActiveRecord::Migrator.migrations_paths = [File.expand_path("../../spec/dummy/db/migrate", __FILE__)]
+
 # require "rails/test_help"
+if defined?(ActiveRecord::Base)
+  ActiveRecord::Migration.maintain_test_schema!
+end
 
 require 'rspec/its'
 
