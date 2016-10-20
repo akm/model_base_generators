e3897e019a3b8b4692d8980e9a7e7003cb00cac8
akm <akm2000@gmail.com>
2016-10-20 18:23:58 +0900

    Fix generated spec for template of issues#index

--- spec/dummy/spec/views/issues/index.html.erb_spec.rb
+++ spec/dummy/spec/views/issues/index.html.erb_spec.rb
@@ -5,16 +5,17 @@ RSpec.describe "issues/index", type: :view do
   let(:user){ FactoryGirl.create(:user) }
   before(:each) do
     assign(:issues, [
-      FactoryGirl.create(:issue, title: 'issue_title1', project: project, creator: user),
-      FactoryGirl.create(:issue, title: 'issue_title2', project: project, creator: user),
+      FactoryGirl.create(:issue, title: 'issue1', project: project, creator: user),
+      FactoryGirl.create(:issue, title: 'issue2', project: project, creator: user),
     ])
   end
 
   it "renders a list of issues" do
     render
-    assert_select "tr>td", :text => 2.to_s, :count => 2
-    assert_select "tr>td", :text => "Title".to_s, :count => 2
-    assert_select "tr>td", :text => 3.to_s, :count => 2
-    assert_select "tr>td", :text => 4.to_s, :count => 2
+    assert_select "tr>td", :text => 'project1', :count => 2
+    assert_select "tr>td", :text => 'user1@example.com', :count => 2
+    assert_select "tr>td", :text => 'issue1', :count => 1
+    assert_select "tr>td", :text => 'issue2', :count => 1
+    assert_select "tr>td", :text => 'Draft', :count => 2
   end
 end
