9db96fec086956f7c87656778b49ee46e0ee6258
akm <akm2000@gmail.com>
2016-10-20 18:23:29 +0900

    Add created_at and updated_at only to issues

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 20161013025452) do
     t.string :title, null: false
     t.integer :status, null: false
     t.references :creator, null: false
+    t.datetime :created_at,                          null: false
+    t.datetime :updated_at,                          null: false
   end
   add_foreign_key :issues, :users, column: 'creator_id'
 end
