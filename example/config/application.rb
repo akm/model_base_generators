29b32099b6b757dbe8e1947477fce803e95e6e51
akm <akm2000@gmail.com>
2016-10-21 08:34:08 +0900

    Add generators configurations to example app

--- example/config/application.rb
+++ example/config/application.rb
@@ -10,6 +10,11 @@ module Dummy
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.generators do |g|
+      g.test_framework  :rspec
+      g.factory_girl dir: 'spec/factories'
+    end
   end
 end
 
