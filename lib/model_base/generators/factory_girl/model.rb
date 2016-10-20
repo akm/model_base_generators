8634a2c7d26089673384ad5d26ac269b18fbe864
akm <akm2000@gmail.com>
2016-10-21 08:26:07 +0900

    Override factory_girl:model generator

--- lib/model_base/generators/factory_girl/model.rb
+++ lib/model_base/generators/factory_girl/model.rb
@@ -9,18 +9,27 @@ module ModelBase
       module Model
         def self.enable!
           ::FactoryGirl::Generators::ModelGenerator.prepend(self)
-          ::FactoryGirl::Generators::ModelGenerator.module_eval do
-            source_paths << File.expand_path("../../../../templates/helper/templates", __FILE__)
+          ::FactoryGirl::Generators::ModelGenerator.extend(ClassMethod)
+          ::FactoryGirl::Generators::ModelGenerator.instance_eval do
+            source_root File.expand_path("../../../../templates/helper/templates", __FILE__)
+          end
+        end
+
+        module ClassMethod
+          # To overwrite `source_root` again, which is overwritten by factory_girl_rails
+          # https://github.com/thoughtbot/factory_girl_rails/blob/master/lib/generators/factory_girl.rb#L6-L8
+          # https://github.com/rails/rails/blob/master/railties/lib/rails/generators/base.rb#L22-L26
+          def source_root(path=nil)
+            @_source_root = path if path
+            @_source_root ||= default_source_root
           end
         end
 
         private
 
         def create_factory_file
-          file = File.join(options[:dir], "#{filename}.rb")
-          create_file(file, single_file_factory_definition)
+          template 'factory.rb', File.join(options[:dir], "#{filename}.rb")
         end
-
 
       end
     end
--- lib/templates/factory_girl/model.rb
+++
@@ -1,7 +0,0 @@
-FactoryGirl.define do
-  factory :<%= model.full_resource_name %> do
-<%- model.columns_for(:factory).each do |col| -%
-
-<%- end -%
-  end
-end
