51ca2933256787cb6abd3bb8c6de25ee9ac4a9c0
akm <akm2000@gmail.com>
2016-10-21 09:13:37 +0900

    Fix generated factory to pass spec

--- example/spec/factories/issues.rb
+++ example/spec/factories/issues.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :issue do
     association :project, factory: :project
-    association :creator, factory: :user
     title "issue1"
     status :draft
+    association :creator, factory: :user
   end
 end
