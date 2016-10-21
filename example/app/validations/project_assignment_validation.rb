4652c196caaa2dc35a7ccd62ce6b857963e93827
akm <akm2000@gmail.com>
2016-10-21 22:48:08 +0900

    Add validation module for ProjectAssignment

---
+++ example/app/validations/project_assignment_validation.rb
@@ -0,0 +1,8 @@
+module ProjectAssignmentValidation
+  extend ActiveSupport::Concern
+
+  included do
+    validates :project_id, presence: true, numericality: true
+    validates :user_id, presence: true, numericality: true
+  end
+end
