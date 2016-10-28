21178f38b353b809942b28a3d263c24977e54112
akm <akm2000@gmail.com>
2016-10-28 18:01:50 +0900

    bundle exec rails g scaffold_controller issue_comments

---
+++ example/spec/requests/issue_comments_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe "IssueComments", type: :request do
+  let(:user) { FactoryGirl.create(:user) }
+  before{ login_as(user, :scope => :user) }
+
+  describe "GET /issue_comments" do
+    it "works! (now write some real specs)" do
+      get issue_comments_path
+      expect(response).to have_http_status(200)
+    end
+  end
+end
