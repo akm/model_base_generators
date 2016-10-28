ce3e09a97b9e1a2eaa59b962d2220c82ad45529b
akm <akm2000@gmail.com>
2016-10-28 10:25:28 +0900

    Add new files for Phase to example

---
+++ example/spec/views/phases/index.html.erb_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe "phases/index", type: :view do
+  let(:project){ FactoryGirl.create(:project, owner: user) }
+  let(:user){ FactoryGirl.create(:user) }
+  before(:each) do
+    assign(:phases, [
+      FactoryGirl.create(:phase, name: 'phase1', project: project),
+      FactoryGirl.create(:phase, name: 'phase2', project: project),
+    ])
+  end
+
+  it "renders a list of phases" do
+    render
+    assert_select "tr>td", :text => 'project1', :count => 2
+    assert_select "tr>td", :text => 'phase1', :count => 1
+    assert_select "tr>td", :text => 'phase2', :count => 1
+    assert_select "tr>td", :text => localize(Time.zone.parse('2020-01-29 02:50:00')), :count => 2
+    assert_select "tr>td", :text => localize(Time.zone.parse('2020-01-29 16:40:00')), :count => 2
+  end
+end
---
+++ example/spec/views/phases/show.html.erb_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe "phases/show", type: :view do
+  let(:project){ FactoryGirl.create(:project, owner: user) }
+  let(:user){ FactoryGirl.create(:user) }
+  before(:each) do
+    @phase = assign(:phase, FactoryGirl.create(:phase, project: project))
+  end
+
+  it "renders attributes in <p>" do
+    render
+    expect(rendered).to match(/project1/)
+    expect(rendered).to match(/phase1/)
+    expect(rendered).to match(Regexp.new(Regexp.escape(localize(Time.zone.parse('2020-01-29 02:50:00')))))
+    expect(rendered).to match(Regexp.new(Regexp.escape(localize(Time.zone.parse('2020-01-29 16:40:00')))))
+  end
+end
