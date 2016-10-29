aa27e5312e053eeefdf02218895f1802abb15500
akm <akm2000@gmail.com>
2016-10-29 22:46:58 +0900

    Use single quotes instead of double quotes

--- lib/templates/rspec/scaffold/show_spec.rb
+++ lib/templates/rspec/scaffold/show_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe "<%= ns_table_name %>/show", <%= type_metatag(:view) %> do
+RSpec.describe '<%= ns_table_name %>/show', <%= type_metatag(:view) %> do
   <%= model.factory_girl_let_definitions %>
   before(:each) do
     @<%= ns_file_name %> = assign(:<%= ns_file_name %>, <%= model.factory_girl_to :create %>)
   end
 
-  it "renders attributes in <p>" do
+  it 'renders attributes in <p>' do
     render
 <% model.columns_for(:spec_show).each do |attribute| -%>
     expect(rendered).to match(<%= attribute.sample_value_regexp_exp %>)
