72d243331bb8647aae04eee13a621c9965c31de2
akm <akm2000@gmail.com>
2016-10-29 23:39:39 +0900

    Modify method name to avoid the following rubocop message
    app/controllers/concerns/authentication.rb:9:7: C: Style/AccessorMethodName: Do not prefix writer method names with set_.
      def set_current_user(&block)
          ^^^^^^^^^^^^^^^^

--- lib/generators/model_base/templates/app/controllers/concerns/authentication.rb
+++ lib/generators/model_base/templates/app/controllers/concerns/authentication.rb
@@ -2,11 +2,11 @@ module Authentication
   def self.included(klass)
     klass.class_eval do
       before_action :authenticate_user!
-      around_action :set_current_user
+      around_action :assign_current_user
     end
   end
 
-  def set_current_user(&block)
+  def assign_current_user(&block)
     User.current(current_user, &block)
   end
 end
