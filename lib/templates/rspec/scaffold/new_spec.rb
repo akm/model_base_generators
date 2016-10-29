8bde3edc96f031c41928c3038ab101e667189618
akm <akm2000@gmail.com>
2016-10-30 00:55:08 +0900

    Use let definition for assigned variable

--- lib/templates/rspec/scaffold/new_spec.rb
+++ lib/templates/rspec/scaffold/new_spec.rb
@@ -3,8 +3,9 @@ require 'rails_helper'
 <% output_attributes = model.columns.reject{|attribute| [:datetime, :timestamp, :time, :date].index(attribute.type) } -%>
 RSpec.describe '<%= ns_table_name %>/new', <%= type_metatag(:view) %> do
   <%= model.factory_girl_let_definitions %>
+  <%= model.factory_girl_let_definition(action: :build) %>
   before(:each) do
-    assign(:<%= ns_file_name %>, <%= model.factory_girl_to :build %>)
+    assign(:<%= model.full_resource_name %>, <%= model.full_resource_name %>)
   end
 
   it 'renders new <%= ns_file_name %> form' do
