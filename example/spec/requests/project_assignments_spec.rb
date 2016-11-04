79087ed0a745ebb56d6f752e6d358f3086638f82
akm <akm2000@gmail.com>
2016-11-05 00:41:19 +0900

    Use single quotes instead of double quotes

--- example/spec/requests/project_assignments_spec.rb
+++ example/spec/requests/project_assignments_spec.rb
@@ -1,9 +1,13 @@
+# frozen_string_literal: true
 require 'rails_helper'
 
-RSpec.describe "ProjectAssignments", type: :request do
-  describe "GET /project_assignments" do
-    it "works! (now write some real specs)" do
-      get project_assignments_index_path
+RSpec.describe 'ProjectAssignments', type: :request do
+  let(:user) { FactoryGirl.create(:user) }
+  before { login_as(user, scope: :user) }
+
+  describe 'GET /project_assignments' do
+    it 'works! (now write some real specs)' do
+      get project_assignments_path
       expect(response).to have_http_status(200)
     end
   end
--- example/spec/requests/projects_spec.rb
+++ example/spec/requests/projects_spec.rb
@@ -1,9 +1,13 @@
+# frozen_string_literal: true
 require 'rails_helper'
 
-RSpec.describe "Projects", type: :request do
-  describe "GET /projects" do
-    it "works! (now write some real specs)" do
-      get projects_index_path
+RSpec.describe 'Projects', type: :request do
+  let(:user) { FactoryGirl.create(:user) }
+  before { login_as(user, scope: :user) }
+
+  describe 'GET /projects' do
+    it 'works! (now write some real specs)' do
+      get projects_path
       expect(response).to have_http_status(200)
     end
   end
