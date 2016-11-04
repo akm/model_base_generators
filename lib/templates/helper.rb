d03ec95911a67c04a5a1af062ee66df1b265d113
akm <akm2000@gmail.com>
2016-11-04 23:28:30 +0900

    Add frozen_string_literal comment to helper template

--- lib/templates/helper.rb
+++ lib/templates/helper.rb
@@ -1,3 +1,6 @@
+<%- unless ModelBase.config.frozen_string_literal.nil? -%>
+# frozen_string_literal: <%= ModelBase.config.frozen_string_literal.inspect %>
+<%- end -%>
 <% module_namespacing do -%>
 module <%= class_name %>Helper
 end
