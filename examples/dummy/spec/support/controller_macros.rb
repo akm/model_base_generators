9006550bee73ce0865f99adec781c544b6bd3df9
akm <akm2000@gmail.com>
2018-01-04 18:52:53 +0900

    rails generate model_base:install

---
+++ examples/dummy/spec/support/controller_macros.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+module ControllerMacros
+  def devise_login(key, user)
+    @request.env['devise.mapping'] = Devise.mappings[key]
+    sign_in(user)
+  end
+
+  def devise_user_login(user)
+    devise_login(:user, user)
+  end
+
+  def login_admin
+    before(:each) { devise_login(:admin, FactoryBot.create(:admin)) }
+  end
+
+  def login_user
+    before(:each) { devise_login(:user, FactoryBot.create(:user)) }
+  end
+end
