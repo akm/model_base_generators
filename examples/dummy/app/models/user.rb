7ace22c7cd9a226cad28fbb21ddd9fd09e7fe259
akm <akm2000@gmail.com>
2018-01-04 18:52:47 +0900

    Add User.current_user

--- examples/dummy/app/models/user.rb
+++ examples/dummy/app/models/user.rb
@@ -3,4 +3,24 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  class << self
+    def current_user=(user)
+      Thread.current[:current_user] = user
+    end
+
+    def current_user
+      Thread.current[:current_user]
+    end
+
+    def current(user)
+      orig_user, User.current_user = User.current_user, user
+      begin
+        return yield
+      ensure
+        User.current_user = orig_user
+      end
+    end
+  end
+
 end
